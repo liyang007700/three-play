bbb982915e9e15c18799c4da5605ffeaf21af3ad
liyang.peace <liyang.peace@bytedance.com>
2020-12-16 19:22:30 +0800

    publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 module.exports = {
+  publicPath: '//liyang007700.github.io/three-play/',
   devServer: {
     port: 8080,
     contentBase: path.join(__dirname, 'src/assets/')
