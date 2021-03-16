c75a0f24440710b42ba4a2dcf4b1567e6ad036ce
liyang.peace <liyang.peace@bytedance.com>
2021-03-16 15:54:13 +0800

    fix: publicPath

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,8 @@ module.exports = {
   devServer: {
     port: 8080,
     contentBase: path.join(__dirname, 'src/assets/')
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/three-play/'
+    : '/'
 }
